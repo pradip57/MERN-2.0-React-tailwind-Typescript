176acd5adfb6f0ff6b242f03eaeb562ca604ab64
Pradip <dellpidarp57@gmail.com>
2024-06-15 10:40:26 +0545

    tailwind on way

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LandingPage from './pages/landing/landing.page';
+import LandingPage1 from './pages/landing/landing.page1';
+// import LandingPage from './pages/landing/landing.page';
 
 
 const htmlRoot: HTMLElement = document.getElementById('root') as HTMLElement;
@@ -12,7 +13,8 @@ const RootElem = ReactDOM.createRoot(htmlRoot);
 
 RootElem.render(
   <React.StrictMode>
-    <LandingPage name="Pradip" email="dellpidarp67@gmail.com"></LandingPage>
+    {/* <LandingPage name="Pradip" email="dellpidarp67@gmail.com"></LandingPage> */}
     {/* <LandingPage  /> */}
+    <LandingPage1></LandingPage1>
   </React.StrictMode>
 );
