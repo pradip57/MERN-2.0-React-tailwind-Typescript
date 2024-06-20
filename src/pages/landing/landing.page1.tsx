2e948db64f778dc93194087d5bebaec240f17c47
Pradip <dellpidarp57@gmail.com>
2024-06-20 19:09:46 +0545

    useState practical

--- src/pages/landing/landing.page1.tsx
+++ src/pages/landing/landing.page1.tsx
@@ -3,25 +3,33 @@ import "./landing.page.css";
 import img1 from "../../assets/react.svg";
 
 const LandingPage1 = (): ReactNode => {
-  let [products, setProducts] = useState([]);
-  let [loading, setLoading] = useState();
+  // let [products, setProducts] = useState([]);
+  // let [loading, setLoading] = useState();
 
   // setProducts([{}]);
-  useEffect(() => {
-    console.log("I am always called");
-  });
+  // useEffect(() => {
+  //   console.log("I am always called");
+  // });
 
-  useEffect(() => {
-    console.log("I am only called when componenet is loaded");
-    setTimeout(() => {
-      // setProducts(true);
-      setLoading(true)
-    }, 3000);
-  }, []);
+  // useEffect(() => {
+  //   console.log("I am only called when componenet is loaded");
+  //   setTimeout(() => {
+  //     // setProducts(true);
+  //     setLoading(true)
+  //   }, 3000);
+  // }, []);
 
-  useEffect(() => {
-    console.log("I am only called when products state  is changed");
-  }, [products]);
+  // useEffect(() => {
+  //   console.log("I am only called when products state  is changed");
+  // }, [products]);
+
+  let [toggleMenu, setToggleMenu] = useState(true);
+
+  const showDropDown = (e: any) => {
+    e.preventDefault();
+    toggleMenu = !toggleMenu;
+    setToggleMenu(toggleMenu);
+  };
 
   return (
     <React.Fragment>
@@ -129,6 +137,7 @@ const LandingPage1 = (): ReactNode => {
               <div className="relative ml-3">
                 <div>
                   <button
+                    onClick={showDropDown}
                     type="button"
                     className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     id="user-menu-button"
@@ -146,7 +155,9 @@ const LandingPage1 = (): ReactNode => {
                 </div>
 
                 <div
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  className={`${
+                    toggleMenu ? "hidden" : ""
+                  } absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
                   role="menu"
                   aria-orientation="vertical"
                   aria-labelledby="user-menu-button"
@@ -218,18 +229,6 @@ const LandingPage1 = (): ReactNode => {
 
       <div className="bg-white ">
         <div className="relative isolate px-6 lg:px-8">
-          <div
-            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-            ></div>
-          </div>
           <div className="mx-auto max-w-2xl py-32 sm:py-48">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
