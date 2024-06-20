142ceaf718189ab3edefb128d075da02a7ccf237
Pradip <dellpidarp57@gmail.com>
2024-06-20 16:58:33 +0545

    learning about state and hook

--- src/pages/landing/landing.page1.tsx
+++ src/pages/landing/landing.page1.tsx
@@ -1,11 +1,31 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import "./landing.page.css";
 import img1 from "../../assets/react.svg";
 
 const LandingPage1 = (): ReactNode => {
+  let [products, setProducts] = useState([]);
+  let [loading, setLoading] = useState();
+
+  // setProducts([{}]);
+  useEffect(() => {
+    console.log("I am always called");
+  });
+
+  useEffect(() => {
+    console.log("I am only called when componenet is loaded");
+    setTimeout(() => {
+      // setProducts(true);
+      setLoading(true)
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    console.log("I am only called when products state  is changed");
+  }, [products]);
+
   return (
     <React.Fragment>
-      <nav className="bg-gray-800">
+      <nav className="bg-lime-50">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -22,13 +42,13 @@ const LandingPage1 = (): ReactNode => {
                   className="block h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                   />
                 </svg>
@@ -37,13 +57,13 @@ const LandingPage1 = (): ReactNode => {
                   className="hidden h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
@@ -57,26 +77,26 @@ const LandingPage1 = (): ReactNode => {
                 <div className="flex space-x-4">
                   <a
                     href="#"
-                    className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                    className="rounded-md bg-slate-300 px-3 py-2 text-sm font-medium text-black"
                     aria-current="page"
                   >
                     Dashboard
                   </a>
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                   >
                     Team
                   </a>
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                   >
                     Projects
                   </a>
                   <a
                     href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                   >
                     Calendar
                   </a>
@@ -86,7 +106,7 @@ const LandingPage1 = (): ReactNode => {
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <button
                 type="button"
-                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                className="relative rounded-full bg-red-900 p-1 text-gray-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
                 <span className="absolute -inset-1.5"></span>
                 <span className="sr-only">View notifications</span>
@@ -94,13 +114,13 @@ const LandingPage1 = (): ReactNode => {
                   className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
                   />
                 </svg>
