5539fe3edcda233788abb24b6f93bc9e3c898881
Pradip <dellpidarp57@gmail.com>
2024-06-23 18:15:33 +0545

    route for login registration just practice

--- src/config/router.config.tsx
+++ src/config/router.config.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage1 from "../pages/landing/landing.page1";
 import Team from "../pages/team";
+import Login from "../pages/auth/login";
+import Register from "../pages/auth/register";
+
 
 const RouterConfig = () => {
   return (
@@ -8,6 +11,8 @@ const RouterConfig = () => {
       <Routes>
         <Route path="/" element={<LandingPage1 />}></Route>
         <Route path="/team" element={<Team />}></Route>
+        <Route path="/auth/login" element={<Login />}></Route>
+        <Route path="/auth/register" element={<Register />}></Route>
 
         <Route path="*" element={<p>Not found</p>}></Route>
       </Routes>
